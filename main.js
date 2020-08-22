5a9bc641b0a6db642029567e2d9017b651aea62f
lnmsv <55012919+lnmsv@users.noreply.github.com>
2020-08-22 17:07:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const $btn = document.getElementById('btn-kick');
 
+const character = {
+  name: 'Pikachu',
+  defaultHP: 100,
+  damageHP: 100,
+}
+
+const enemy = {
+  name: 'Charmader',
+  defaultHP: 100,
+  damageHP: 100,
+}
+
+$btn.addEventListener('click', function() {
+  console.log('Kick');
+})
